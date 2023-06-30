b0d62906379a5a75f4c84bcc952b147d21971f0c
Azeez Babalola <109736499+azeezrel@users.noreply.github.com>
2023-06-30 14:32:45 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conn = mysqli_connect('phpmysqldb.mysql.database.azure.com','azeez','Babalola@1234','phpdb');
+	$conn = mysqli_connect('phpmysqldb.mysql.database.azure.com','azeez','Babalola@1234','musical_world');
 
 	if(!$conn)
 		die("Error while connecting...!").mysqli_connect_error($conn);
